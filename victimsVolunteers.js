1c5ff972f61f7bcb15d20658f7a8c184ea60207c
Tim Osborn <tim@601am.com>
2015-05-26 23:08:43 -0600

    initiak commit for Victims and Volunteers

---
+++ victimsVolunteers.js
@@ -0,0 +1,10 @@
+var victimTotal = prompt("How many disaster victims are you entering?");
+
+//var victimNumber = victimTotal * 1;
+var victimInfo = function() {	
+	var victimNumber = victimTotal * 1;
+	for (var i = 0; i < victimNumber; i++) {
+		var vicName = prompt("What is the victims name?");				
+	}
+};
+victimInfo();
